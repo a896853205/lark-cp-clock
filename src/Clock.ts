3b21d08c6ec35de5ef94a12fa3a5caa962ea3ac5
Eric <896853205@qq.com>
2021-07-31 15:37:45 +0800

    init: 初始化项目结构

---
+++ src/Clock.ts
@@ -0,0 +1,11 @@
+class Clock {
+  clockAlarm: ClockAlarm = new ClockAlarm();
+
+  constructor (clockAlarm: ClockAlarm) {
+    this.clockAlarm = clockAlarm;
+  }
+
+  private getNextExpectTime () {
+    
+  }
+}
---
+++ src/ClockAlarm.ts
@@ -0,0 +1,8 @@
+class ClockAlarm {
+  circulateTimes: number = 1;
+  message: string = '';
+  alarmStartTime: number = Number(new Date());
+  timeInterval: number = 1;
+  ranTimes: number = 0;
+  isWorkDay: boolean = false;
+}
